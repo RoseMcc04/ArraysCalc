e15f021af24f1bbe75e16637c831ccf0dd351428
RoseMcc04 <mccormacka@appstate.edu>
2024-12-11 22:54:55 -0500

    creating a new header file

---
+++ src/main/cpp/edu/appstate/cs/ArraysCalc/ArraysCalc.hpp
@@ -0,0 +1,13 @@
+#ifndef ARRAYSCALC_HPP
+#define ARRAYSCALC_HPP
+
+/**
+ * @brief This class holds the primary array and 2D array calculator
+ * functions. 
+ */
+class ArrayCalc 
+{
+
+};
+
+#endif ARRAYSCALC_HPP
