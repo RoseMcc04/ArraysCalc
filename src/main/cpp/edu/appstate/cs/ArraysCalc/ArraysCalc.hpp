c7b7442e7f3ba868bd5176f2bd4bf6a22179a5f0
RoseMcc04 <mccormacka@appstate.edu>
2024-12-11 23:05:52 -0500

    still working on header file

--- src/main/cpp/edu/appstate/cs/ArraysCalc/ArraysCalc.hpp
+++ src/main/cpp/edu/appstate/cs/ArraysCalc/ArraysCalc.hpp
@@ -1,13 +1,25 @@
 #ifndef ARRAYSCALC_HPP
 #define ARRAYSCALC_HPP
 
+#include <iostream>
+#include <cstddef>
+#include <cstdlib.h>
+
 /**
  * @brief This class holds the primary array and 2D array calculator
  * functions. 
  */
 class ArrayCalc 
 {
+    private:
 
+    /**
+     * @brief This function calculates the sum of a 1D array.
+     * 
+     * @param arr 1D array whose sum is being calculated.
+     */
+    template <typename T> T calcSum(T arr[]);
+    template <typename T, size_t N> T calcSum(T arr[][N], size_t rows);
 };
 
 #endif ARRAYSCALC_HPP
