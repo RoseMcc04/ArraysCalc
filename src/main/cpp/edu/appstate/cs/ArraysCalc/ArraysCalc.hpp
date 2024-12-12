1263fb842150512c5296b534fcbe87e12172f1d9
RoseMcc04 <mccormacka@appstate.edu>
2024-12-12 00:31:55 -0500

    .vscode/c_cpp_properties.json --> Needed the configuration path for the compiler src/main/cpp/edu/appstate/cs/ArraysCalc/ArraysCalc.hpp --> Creating template for functions

--- src/main/cpp/edu/appstate/cs/ArraysCalc/ArraysCalc.hpp
+++ src/main/cpp/edu/appstate/cs/ArraysCalc/ArraysCalc.hpp
@@ -3,11 +3,13 @@
 
 #include <iostream>
 #include <cstddef>
-#include <cstdlib.h>
 
 /**
  * @brief This class holds the primary array and 2D array calculator
  * functions. 
+ * 
+ * @author Rose McCormack
+ * @version 2025
  */
 class ArrayCalc 
 {
@@ -17,8 +19,37 @@ class ArrayCalc
      * @brief This function calculates the sum of a 1D array.
      * 
      * @param arr 1D array whose sum is being calculated.
+     * @return Returns the sum of the array.
      */
     template <typename T> T calcSum(T arr[]);
+
+    /**
+     * @brief This function calculates the product of a 1D array.
+     * 
+     * @param arr 1D array whose product is being calculated.
+     * @return Returns the product of the array.
+     */
+    template <typename T> T calcProduct(T arr[]);
+
+    /**
+     * @brief This function returns the maximum value in a 1D array.
+     * 
+     * @param arr 1D array whose max is being searched.
+     * @return Returns the maximum value found in the array.
+     */
+    template <typename T> T calcMax(T arr[]);
+
+    /**
+     * @brief This function returns the minimum value in a 1D array.
+     * 
+     * @param arr 1D array whose min is being searched.
+     * @return Returns the minimum value found in the array.
+     */
+    template <typename T> T calcMin(T arr[]);
+
+    /**
+     * @brief This function calculates the sum of a 2D array.
+     */
     template <typename T, size_t N> T calcSum(T arr[][N], size_t rows);
 };
 
